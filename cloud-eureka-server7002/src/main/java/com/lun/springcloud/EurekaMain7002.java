bdf9c38d2eebcda8e64bd697d539d7445bf91625
fuqinderen2 <2396983681@qq.com>
2022-04-04 09:27:52 +0800

    推送测试

--- cloud-eureka-server7002/src/main/java/com/lun/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/lun/springcloud/EurekaMain7002.java
@@ -13,5 +13,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaMain7002 {
     public static void main(String[] args) {
         SpringApplication.run(EurekaMain7002.class, args);
+        System.out.println("test");
     }
 }
